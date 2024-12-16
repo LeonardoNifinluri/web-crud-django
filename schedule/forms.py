d3f983fa36bbfad7889ad49623387b7fda278cf6
winati17 <winatimn@gmail.com>
2024-12-16 17:24:55 +0800

    feat : dropdown for days & rooms

--- schedule/forms.py
+++ schedule/forms.py
@@ -1,10 +1,44 @@
 from django import forms
 from schedule.models import Major, Schedule, Subject
 
-
 class ScheduleForm(forms.ModelForm):
-    major = forms.ModelChoiceField(queryset=Major.objects.all(), required=True, empty_label="Select Major")
-    subject = forms.ModelChoiceField(queryset=Subject.objects.all(), required=True, empty_label="Select Subject")
+    major = forms.ModelChoiceField(
+        queryset=Major.objects.all(),
+        required=True,
+        empty_label="Select Major",
+        widget=forms.Select(attrs={'class': 'dropdown'})
+    )
+    subject = forms.ModelChoiceField(
+        queryset=Subject.objects.all(),
+        required=True,
+        empty_label="Select Subject",
+        widget=forms.Select(attrs={'class': 'dropdown'})
+    )
+    teacher_name = forms.CharField(
+        required=True,
+        widget=forms.TextInput(attrs={'placeholder': 'Select teacher', 'class': 'text-input'})
+    )
+    time_begins = forms.TimeField(
+        required=True,
+        widget=forms.TimeInput(attrs={'type': 'time', 'class': 'time-input'})
+    )
+    time_ends = forms.TimeField(
+        required=True,
+        widget=forms.TimeInput(attrs={'type': 'time', 'class': 'time-input'})
+    )
+
+    room = forms.ChoiceField(
+        choices=[('', 'Select Room')] + Schedule.ROOM_CHOICES,
+        required=True,
+        widget=forms.Select(attrs={'class': 'dropdown'})
+    )
+
+    day = forms.ChoiceField(
+        choices=[('', 'Select Day')] + Schedule.DAY_CHOICES,
+        required=True,
+        widget=forms.Select(attrs={'class': 'dropdown'})
+    )
+
     class Meta:
         model = Schedule
         fields = ['major', 'subject', 'teacher_name', 'time_begins', 'time_ends', 'room', 'day']
